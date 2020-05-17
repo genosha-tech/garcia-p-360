f9dce8264e08f8fa1ca2df00ca979d126fb2a43f
Your Name <you@example.com>
2020-05-17 17:38:11 +0200

    a

--- components/set-image.js
+++ components/set-image.js
@@ -45,13 +45,13 @@ AFRAME.registerComponent('set-image', {
     targetEl.dataset.setImageFadeSetup = true;
 
     // Create animation.
-    targetEl.setAttribute('animation__fade', {
-      property: 'material.color',
-      startEvents: 'set-image-fade',
-      dir: 'alternate',
-      dur: data.dur,
-      from: '#FFF',
-      to: '#000'
-    });
+    // targetEl.setAttribute('animation__fade', {
+    //   property: 'material.color',
+    //   startEvents: 'set-image-fade',
+    //   dir: 'alternate',
+    //   dur: data.dur,
+    //   from: '#FFF',
+    //   to: '#000'
+    // }); 
   }
 });
